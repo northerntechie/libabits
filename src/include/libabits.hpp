a981cb8cd2920a2186fe3e1d96044abb4c9f6d94
Todd Saharchuk <todd.saharchuk@gmail.com>
2018-10-31 15:50:09 -0700

    20181031 - Ongoing development.  Added Doxyfile.

--- src/include/libabits.hpp
+++ src/include/libabits.hpp
@@ -27,6 +27,7 @@
 #include <cassert>
 #include <bitset>
 #include <vector>
+#include <map>
 #include <string>
 #include <sstream>
 #include <utility>
@@ -38,7 +39,7 @@ namespace abits {
   namespace num_type {
     // Numeral system type constants
     // Integral value is the number of bits, length of bit field
-    const int Base2 = 1;
+    constexpr int Base2 = 1;
     const int binary = Base2;
     const int Base4 = 2;
     const int quaternary = Base4;
@@ -55,14 +56,14 @@ namespace abits {
   // Base64 code to char (ASCII) constexpr map
   // All lower bases are subsets of the Base64
   // numeral type.
-  constexpr std::pair<int,int> map_code_char[] =
-    {
-      {0,'A'},{1,'B'},{2,'C'},{3,'D'},{4,'E'},
-      {5,'F'},{6,'G'},{7,'H'},{8,'I'},{9,'J'},
-      {10,'K'},{11,'L'},{12,'M'},{13,'N'},{14,'O'},
-      {15,'P'},{16,'Q'},{17,'R'},{18,'S'},{19,'T'},
-      {20,'U'},{21,'V'},{22,'W'},{23,'X'},{24,'Y'},
-      {25,'Z'},{26,'a'},{27,'b'},{28,'c'},{29,'d'},
+  const std::map<int,char> map_code_char =
+    {                                               
+      {0,'A'},{1,'B'},{2,'C'},{3,'D'},{4,'E'},      
+      {5,'F'},{6,'G'},{7,'H'},{8,'I'},{9,'J'},      
+      {10,'K'},{11,'L'},{12,'M'},{13,'N'},{14,'O'}, 
+      {15,'P'},{16,'Q'},{17,'R'},{18,'S'},{19,'T'}, 
+      {20,'U'},{21,'V'},{22,'W'},{23,'X'},{24,'Y'}, 
+      {25,'Z'},{26,'a'},{27,'b'},{28,'c'},{29,'d'}, 
       {30,'e'},{31,'f'},{32,'g'},{33,'h'},{34,'i'},
       {35,'j'},{36,'k'},{37,'l'},{38,'m'},{39,'n'},
       {40,'o'},{41,'p'},{42,'q'},{43,'r'},{44,'s'},
@@ -73,23 +74,37 @@ namespace abits {
     };
   constexpr int map_size = sizeof(map_code_char) / sizeof(std::pair<int,int>);
 
-  constexpr static char getChar(int code, int range = map_size-1)
+  const static char get_char(int code, int range = map_size-1)
   {
     assert(range > -1 && range < map_size);
-    return
-      (code == map_code_char[range].first) ?
-      map_code_char[range].second :
-      getChar(code,range-1);
+    auto it = map_code_char.find(code);
+    if(it != map_code_char.end())
+      return it->second;
+    else
+      {
+	assert(false);
+	return 0x00;
+      }
   }
 
-  const static int getCode(char character, int range = map_size-1)
+  const static int get_code(char character, int range = map_size-1)
   {
     assert(range > -1 && range < map_size);
-    return
-      (character == map_code_char[range].second) ?
-      map_code_char[range].first :
-      getCode(character,range-1);
+    auto it = std::find_if(map_code_char.begin(),
+			   map_code_char.end(),
+			   [&character] (const std::pair<int,char>& pair) -> bool
+			   {
+			     return pair.second == character;
+			   });
+    if(it != map_code_char.end())
+      return it->first;
+    else
+      {
+	assert(false);
+	return -1;
+      }
   }
+  
   constexpr static int mask[] =
     { 0x00,  /* Base0 */
       0x01,  /* Base2 */
@@ -101,12 +116,12 @@ namespace abits {
     };
   
   /* Forward declarations */
-  template<int N, int T = num_type::Base16>
-    class BaseEnc;
-
-  template<int N>
-    std::ostream& operator<<(std::ostream& os,
-			     const BaseEnc<N>& obj);
+  template<int T = num_type::Base16>
+  class base_enc;
+  
+  template<int T>
+  std::ostream& operator<<(std::ostream& os,
+			   const base_enc<T>& obj);
   /* End of forward declarations */
   
   /**
@@ -114,81 +129,125 @@ namespace abits {
    * public interface to the underlying bitset storage of the encoded
    * integer value.
    */
-  template<int N, int T>
-  class BaseEnc
+  template<int T>
+  class base_enc
   {
   private:
-    std::bitset<N*T> store;
-    std::string toString() const
-      {
-	return "toString() method call..."s;
-      };
-    std::bitset<N*T> stringToBits(const std::string str)
-      {
-	std::string revStr{str};
-	assert(str.size() <= N);
-	std::bitset<N*T> ret;
+    std::vector<bool> _store{};
 
-	std::reverse(revStr.begin(),revStr.end());
-
-	for(auto ch : revStr)
-	  {
-	    int ichar = getCode(ch);
-	    std::cout<< "ichar= "
-		     << std::hex << ichar << "\n";
-	    ret |= ichar;
-	    ret <<= T;
-	  }
-	
-	return ret;
-      };
-
-    std::string bitsToString()
-      {
-	std::stringstream ss;
-
-	int i=N;
-	while(i>0)
-	  {
-	    --i;
-	    char ch = (char)(store & mask[T]);
-	    ss << ch;
-	    store <<= T;
-	  }
-	return ss.str();
-      };
+    /**
+     * char_insert inserts the binary underlying
+     * representation least-bit first.
+     */
+    std::vector<bool>& char_insert(char ch, std::vector<bool>& vec)
+    {
+      int chh = ch;
+      for(int i=0; i<T; i++)
+	{
+	  vec.push_back(chh & 0x01);
+	  chh >>= 1;
+	}
+      return vec;
+    };
     
   public:
-    // TODO(Todd): implement a default cstor
-    BaseEnc() {};
-    BaseEnc(const std::string value)
+    // TODO(Todd): implement a default ctor
+    base_enc() {};
+    base_enc(const std::string value)
       {
 	assert(T >= num_type::Base2 && T <= num_type::Base64);
-	if(value.size() != N) throw("Bit limb size and string do no match");
-	store = stringToBits(value);
+	_store = string_to_bits(value);
       };
-    int getBitLength()
+    int get_bitlength()
     {
-      return store.size();
-    }
-    int getCharLength()
+      return _store.size();
+    };
+    
+    int get_charlength()
     {
-      return N;
-    }
-    std::bitset<T*N> getBitset()
-      {
-	return store;
-      }
+      return T;
+    };
+
+    /**
+     * This method produces a string representation of
+     * the bit storage of the encoded string.
+     *
+     * Complexity: O(n) where n is the bit length of the
+     * bit storage.
+     *
+     * @return a string representation of the bit storage.
+     */
+    std::string bitstring()
+    {
+      std::stringstream ss;
+
+      for(bool b : _store)
+	{
+	  ss << (b == true) ? 1 : 0;
+	}
+      return ss.str();
+    };
+
+    /**
+     * This method produces a vector<bool> object
+     * derived from the supplied std::string.
+     *
+     *
+     * @param str a std::string used to generate the 
+     * bit representation using the underlying class
+     * numeral type.
+     */
+    std::vector<bool> string_to_bits(const std::string& str)
+    {
+      std::vector<bool> ret;
+      std::string revStr{str};
+      std::reverse(revStr.begin(),revStr.end());
+      
+      for(auto ch : revStr)
+	{
+	  ret = char_insert(get_code(ch), ret);
+	}
+      return ret;
+    };
+    
+    /**
+     * This function converts the internal bit storage
+     * to a string representation using the object
+     * underlying numeral type.
+     *
+     * 
+     */
+    std::string bits_to_string() const
+    {
+      std::stringstream ss;
+      std::vector<bool> bits{_store};
+      
+      int n = _store.size() / T;
+      while(n>0)
+	{
+	  int ch = 0;
+	  for(int i=0; i<T; i++)
+	    {
+	      ch <<= 1;
+	      ch |= bits.back();
+	      bits.pop_back();
+	    }
+	  ss << get_char(ch);
+	  n--;
+	}
+      
+      return ss.str();
+    };
+    
     friend std::ostream& operator<< <>(std::ostream& os,
-				       const BaseEnc<N>& obj);
+				       const base_enc<T>& obj);
   };
   
   template<int N>
     std::ostream& operator<<(std::ostream& os,
-			     const BaseEnc<N>& obj)
+			     const base_enc<N>& obj)
     {
-      std::string out{obj.bitsToString()};
-      os << out;
+      os << obj.bits_to_string();
       return os;
     }
 }
--- src/tests/libabits_tests.cpp
+++ src/tests/libabits_tests.cpp
@@ -10,24 +10,26 @@
  * unit test terminates.
  */
 #include <iostream>
-#include <libabits.h>
+#include <libabits.hpp>
 
 int main()
 {
   // Construct an BaseEnc
-  std::cout << "BaseEnc(\"Hello\"s)= " ;
-  abits::BaseEnc<4,abits::num_type::hexadecimal> string1{"help"};
+  std::cout << "base_enc(\"help\"s)= " ;
+  abits::base_enc<abits::num_type::Base64> string1{"Thisisatestofthecapabilitiesoftheabitslibrary"};
   std::cout << string1 << "\n";
-  std::cout << "string1.getBitLength()= "
-	    << string1.getBitLength() << "\n";
-  std::cout << "string1.getCharLength()= "
-	    << string1.getCharLength() << "\n";
-  std::cout << "static getChar()test...\n"
-	    << "pos=23: " << abits::getChar(23) << "\n";
-  std::cout << "getCode('H')= "
-	    << abits::getCode('H') << "\n";
+  std::cout << "string1.get_bitlength()= "
+	    << string1.get_bitlength() << "\n";
+  std::cout << "string1.get_charlength()= "
+	    << string1.get_charlength() << "\n";
+  std::cout << "static get_char()test...\n"
+	    << "pos=23: " << abits::get_char(23) << "\n";
+  std::cout << "get_code('H')= "
+	    << abits::get_code('H') << "\n";
   std::cout << "bitset of string1= "
-	    << string1.getBitset().to_string() << "\n";
+	    << string1.bitstring() << "\n";
+  std::cout << "string of string1= "
+	    << string1 << "\n";
   //std::cout << "pos=64(out of bounds): " // error
   // << abits::getChar(64) << "\n";
 }
